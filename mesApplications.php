a16ec0911c0d851d97ea4c642032e243fe88ac8a
Lelu <sorin.luc@gmail.com>
2015-05-20 17:46:32 +0200

    correction bug dans mesApps

--- mesApplications.php
+++ mesApplications.php
@@ -19,16 +19,17 @@ $login=$_SESSION['login'];
 $querystring="SELECT * FROM (client c INNER JOIN produit_achete pa ON c.login=pa.proprietaire)R1
                             INNER JOIN v_application va ON R1.produit=va.titre
                             WHERE R1.login='$login'"; //Requete du turfu by LeLu #checkdisout
-$query=pg_query($querystring);
+$query=pg_query($idConnex,$querystring);
 $res=pg_fetch_array($query);
 
 if (is_null($res['titre'])){
     echo "<p> Vous ne possédez pas encore d'application! Trouvez dès maintenant celle de vos rêves grâce à l'outil recherche !</p>";
 }else{
-    while($res){
+    while(!is_null($res['titre'])){
         echo "<p>Nom : ".$res['titre']."</p>";
         echo "<p>Editeur : ".$res['editeur']."</p>";
         echo "</br>";
+        $res=pg_fetch_array($query);
     }
 }
 ?>
