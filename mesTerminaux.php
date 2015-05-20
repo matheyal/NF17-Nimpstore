09ed5c507813a3e7ae42d2e21a64d1909a965118
Lelu <sorin.luc@gmail.com>
2015-05-20 17:44:18 +0200

    ajout de terminaux et affichage des terminaux fonctionnels

--- mesTerminaux.php
+++ mesTerminaux.php
@@ -1,6 +1,34 @@
 <?php session_start();
 $login=$_SESSION['login'];
 ?>
+
+<?php
+if (isset($_POST['ajout'])){
+    include("idConnex.php");
+    $num=$_POST['num_serie'];
+    $modele=$_POST['modele'];
+    
+    $querystring="SELECT numero_serie FROM terminal t WHERE t.numero_serie='$num'";
+    $query=pg_query($idConnex,$querystring);
+    $res=pg_fetch_array($query);
+    if (!is_null($res['numero_serie'])){
+    }
+    else{
+    
+    $querystring="SELECT m.id as mid,os.id as osid FROM modele m INNER JOIN systeme_exploitation os ON m.os=os.id
+                    WHERE m.designation='$modele'";
+    $query=pg_query($querystring);
+    $res=pg_fetch_array($query);
+    $mid=$res['mid'];
+    
+    $query=pg_query("INSERT INTO terminal VALUES ('$num','$mid','$login')")
+            or die("erreur lors de l'insertion, contacter l'administrateur");
+
+    header("Location:index.php");
+    }
+    
+}
+?>
 <html>
 <head>
     <title>NimpStore - Mes Applications</title>
@@ -16,18 +44,20 @@ $login=$_SESSION['login'];
 <h1>Vos Terminaux <?php echo $login; ?></h1>
 
 <?php 
-$querystring="SELECT * FROM client c INNER JOIN terminal t ON c.login=t.proprietaire
-                WHERE c.login='$login'";//Requete du turfu by LeLu #checkdisout
-$query=pg_query($querystring);
+$querystring="SELECT R1.numero_serie,m.designation FROM (client c INNER JOIN terminal t ON c.login=t.proprietaire)R1
+                INNER JOIN modele m ON R1.modele=m.id
+                WHERE R1.login='$login'";//Requete du turfu by LeLu #checkdisout
+$query=pg_query($idConnex,$querystring);
 $res=pg_fetch_array($query);
 
-if (is_null($res['modele'])){
+if (is_null($res['designation'])){
 echo "<p>Vous n'avez pas encore renseigné d'informations concernant vos appareils! Vous pourrez bientôt en ajouter autrement qu'en demandant à Alexis Mathey de modifier les tables ! #comingsoon</p>";
 }else{
-    while($res){
-        echo "<p>Modèle : ".$res['modele']."</p>";
+    while(!is_null($res['designation'])){
+        echo "<p>Modèle : ".$res['designation']."</p>";
         echo "<p>N° Série : ".$res['numero_serie']."</p>";
         echo "</br>";
+        $res=pg_fetch_array($query);
     }
 }
 ?>
@@ -35,8 +65,10 @@ echo "<p>Vous n'avez pas encore renseigné d'informations concernant vos apparei
 <h1>Ajouter un terminal</h1>
 <p>Remplissez le formulaire ci-dessous pour ajouter un nouveau terminal. Vous pourrez ainsi télécharger et installer vos applications favorites sur celui-ci !</p>
 <form method="post" action="mesTerminaux.php">
-    <input type="text" name="num_serie" placeholder="N° Serie">
-    <select name="modele">
+    <input type="text" name="num_serie" placeholder="N° Serie" required>
+    <select name="modele" required>
+    <option disabled>Modèle</option>
+    <option disabled>──────────</option>
     <?php //options
         $querystring = "SELECT designation FROM modele"; //Query pour prendre les noms des éditeurs
         $query = pg_query($idConnex,$querystring);
