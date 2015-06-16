8b374dc3f186239bdb91e36d3b4f7dc673560019
matheyal <alexis.mathey@etu.utc.fr>
2015-06-16 10:17:26 +0200

    update

--- connection.php
+++ connection.php
@@ -10,7 +10,7 @@ if (1==fLogin($login,$pass,$idConnex)) {
     $_SESSION['admin'] = 0;
     header('Location: index.php');
 }
-else if (2==fLogin($login,$pass,$idConnex){  //Ne pas oublier de mettre en place une page spécial admin, qui implique une redirection spéciale
+else if (2==fLogin($login,$pass,$idConnex)) {  //Ne pas oublier de mettre en place une page spécial admin, qui implique une redirection spéciale
     session_start();
     $_SESSION['login'] = $login;
     $_SESSION['alreadyBought'] = 0;
